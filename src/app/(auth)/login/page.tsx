66516e3b7aa9f2d8bc44b3e3922363f645183bf3
shivanisrichippa <shivanisrichippa@gmail.com>
2025-03-25 19:32:33 +0530

    Login page UI

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,62 @@
 'use client';
 
+import Image from 'next/image';
+import { FormEvent } from 'react';
+
 export default function Main() {
+  const handleLoginSubmit = (formEvent: FormEvent<HTMLFormElement>) => {
+    formEvent.preventDefault();
+
+    // Handle form submission logic here
+  };
+
   return (
-    <section className="h-screen bg-black ">
-      <div className="max-w-[280px] mx-auto">
-        <div className="flex flex-col items-center pt-[15vh]">
-          <h2 className="mb-5 text-white font-mono font-bold text-xl">
-            Login In
-          </h2>
+    <section className="flex h-screen items-center  justify-center bg-gray-100">
+      <div className="w-full max-w-md space-y-6 rounded-lg bg-white p-8 shadow-lg">
+        <div className="flex justify-center">
+          <Image 
+            src="/trolla-logo.png" 
+            alt="Trolla Logo" 
+            width={100} 
+            height={100} 
+            priority 
+            style={{ width: 'auto', height: 'auto' }} 
+          />
         </div>
+        <form className="space-y-4" onSubmit={handleLoginSubmit}>
+          <div>
+            <label className="block text-sm py-2 font-medium text-gray-600" htmlFor="email">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              autoComplete="email"
+              className="mt-1 w-full rounded-lg border  border-gray-600 px-4 py-2 focus:ring focus:ring-orange-400 placeholder:text-gray-500"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm py-2 font-medium text-gray-600" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              autoComplete="current-password"
+              className="mt-1 w-full rounded-lg border  border-gray-600 px-4 py-2 focus:ring focus:ring-orange-400 placeholder:text-gray-500"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-lg border border-[#FF4A37] bg-[#FF4A37] py-2 text-white transition hover:bg-white hover:text-[#FF4A37]"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </section>
   );
