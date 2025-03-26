b59bed5b0b46793dc612e06b768dcdbb925795bc
shivanisrichippa <shivanisrichippa@gmail.com>
2025-03-26 17:04:45 +0530

    Login page UI

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+    trolla: {
+      DEFAULT: "#FF4329", // Primary color
+      light: "#FF6A55", // Lighter shade
+      dark: "#D63A24", // Darker shade
+    },
+  };
+  
+  export default colors;
+  
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+import colors from "./src/styles/colors";
+
+const config: Config = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+};
+
+export default config;
