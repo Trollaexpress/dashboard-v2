9b244ef27eb60f2dce135bf0830010507eee7262
shivanisrichippa <shivanisrichippa@gmail.com>
2025-04-07 17:06:19 +0530

    UI login pg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
+// tailwind.config.ts
 import type { Config } from 'tailwindcss';
 import colors from './src/styles/colors'; 
 
 const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
-    
   ],
   theme: {
     extend: {
       colors: {
-        ...colors, 
+        trolla: colors.trolla,
       },
     },
   },
