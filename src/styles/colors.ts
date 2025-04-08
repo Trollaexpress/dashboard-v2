0bd28a823a7e7f5a9029968dd44a773a4ee5f32c
Chippa Shivanisri <154530117+shivanisrichippa@users.noreply.github.com>
2025-04-08 11:30:11 +0530

    Implement Login Page UI with Tailwind CSS Configuration (#7)
    * Login page UI
    
    * Login Ui Updated
    
    * Login UI updated
    
    * Tailwind config
    
    * UI login pg
    
    * login page UI
    
    * Updated login page UI
    
    * updated Login page UI

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#FF4329',
+  trolla: {
+    DEFAULT: '#FF4329', // Primary color
+    light: '#FF6A55', // Lighter shade
+    dark: '#D63A24',
+  },
+};
+
+export default colors;
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+// tailwind.config.ts
+import type {Config} from 'tailwindcss';
+import colors from './src/styles/colors';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        trolla: colors.trolla,
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
